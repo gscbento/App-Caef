89ecc0205696ed08e2c4a28bdcb2555b20d80eee
gscbento <gscbento@gmail.com>
2021-03-29 08:40:41 -0300

    adição da parte de notícias com estrutura exemplo

--- App/lib/home.dart
+++ App/lib/home.dart
@@ -1,45 +1,89 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import '../home.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:maquete/homepage.dart';
 
-void main() => runApp(MyApp());
+//Pages
+import './news.dart';
+import './feedback.dart';
+import './produtos.dart';
+import './about.dart';
+import './contact.dart';
+import './guide.dart';
+import 'backend/rss_2_json.dart';
 
-class MyApp extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  Map<String, List> newsData = Map<String, List>();
+  bool isLoading = true;
+  getData() async {
+    Future.wait([
+      rssToJson('topnews'),
+      rssToJson('india'),
+      rssToJson('world-news'),
+      rssToJson('business'),
+      rssToJson('sports'),
+      rssToJson('cricket'),
+      rssToJson('education'),
+      rssToJson('entertainment'),
+      rssToJson('lifestyle'),
+      rssToJson('health'),
+      rssToJson('books'),
+      rssToJson('trending'),
+    ]).then((value) {
+      value[0] = [];
+      value.forEach((element) {
+        value[0].addAll([...element ?? []]);
+      });
+      value[0].shuffle();
+      newsData['topnews'] = value[0].sublist(0, 10);
+      newsData['india'] = value[1];
+      newsData['world'] = value[2];
+      newsData['business'] = value[3];
+      newsData['sports'] = value[4];
+      newsData['cricket'] = value[5];
+      newsData['education'] = value[6];
+      newsData['entertainment'] = value[7];
+      newsData['lifestyle'] = value[8];
+      newsData['health-fitness'] = value[9];
+      newsData['books'] = value[10];
+      newsData['its-viral'] = value[11];
+      setState(() {
+        isLoading = false;
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Home(),
-    );
-  }
-}
- 
- /*{
-  runApp(new MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.red, brightness: Brightness.dark),
-      home: Home(),);
-}
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => new _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: buildAppBar(), //Appbar de cima
-      drawer: buildDrawer(),
-      home: buildHomePage(),
-      //bottomNavigationBar: buildBottomAppBar(), //AppBar de baixo
-    );
-  }
-
-  Drawer buildDrawer() {
-    return Drawer(
-      child: new Container(
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 25),
+          child: Builder(
+            builder: (context) => IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: () => Scaffold.of(context).openDrawer(),
+            ),
+          ),
+        ),
+        backgroundColor: Colors.grey[850],
+        elevation: 0,
+        centerTitle: false,
+        titleSpacing: 0,
+      ),
+      drawer: Container(
         color: Colors.grey[900],
         child: ListView(
           children: <Widget>[
@@ -134,23 +178,13 @@ class _MyAppState extends State<MyApp> {
           ],
         ),
       ),
+      body: isLoading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : HomePage(
+              newsData: newsData,
+            ),
     );
   }
-  AppBar buildAppBar() {
-    return new AppBar(
-      //automaticallyImplyLeading: false,
-      backgroundColor: Colors.grey[850],
-      //Leading se precisar
-      leading: (
-         Drawer();
-      ),
-      
-        IconButton(
-        icon: new Icon(Icons.menu),
-        onPressed: () {
-          setState();
-        },
-      ),
-    );
-  }
-}*/
+}
--- App/lib/produtos.dart
+++ App/lib/produtos.dart
@@ -13,6 +13,174 @@ class _State extends State<ProductsPage> {
         backgroundColor: Colors.grey[850],
         title: new Text('Produtos'),
       ),
+      body: produtos(),
+    );
+  }
+}
+
+class produtos extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          Container(
+            child: Card(
+              elevation: 7,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              color: Colors.grey[900],
+              shadowColor: Colors.yellow,
+              child: Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Image(
+                      image: AssetImage('img/camiseta.jpeg'),
+                      height: 140.0,
+                    ),
+                    Column(
+                      children: [
+                        Text('Camiseta masculina'),
+                        Text('R\$ 30.00',
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold)),
+                        ButtonBar(
+                          // margin:
+                          alignment: MainAxisAlignment.center,
+                          buttonHeight: 10,
+                          //mainAxisSize: MainAxisSize.max,
+                          buttonPadding: EdgeInsets.all(5),
+                          children: <Widget>[
+                            RaisedButton(
+                              color: Colors.red,
+                              child: Row(
+                                children: [
+                                  Icon(Icons.shopping_basket_outlined),
+                                  Text(' Comprar'),
+                                ],
+                              ),
+                              onPressed: () {},
+                            ),
+                          ],
+                        ),
+                        Text('Disponível'),
+                      ],
+                    ),
+                  ],
+                ),
+                margin: EdgeInsets.all(15.0),
+              ),
+            ),
+            margin: EdgeInsets.all(15.0),
+          ),
+          Divider(),
+          Container(
+            child: Card(
+              elevation: 7,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              color: Colors.grey[900],
+              shadowColor: Colors.yellow,
+              child: Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Image(
+                      image: AssetImage('img/camiseta.jpeg'),
+                      height: 140.0,
+                    ),
+                    Column(
+                      children: [
+                        Text('Camiseta masculina'),
+                        Text('R\$ 30.00',
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold)),
+                        ButtonBar(
+                          // margin:
+                          alignment: MainAxisAlignment.center,
+                          buttonHeight: 10,
+                          //mainAxisSize: MainAxisSize.max,
+                          buttonPadding: EdgeInsets.all(5),
+                          children: <Widget>[
+                            RaisedButton(
+                              color: Colors.red,
+                              child: Row(
+                                children: [
+                                  Icon(Icons.shopping_basket_outlined),
+                                  Text(' Comprar'),
+                                ],
+                              ),
+                              onPressed: () {},
+                            ),
+                          ],
+                        ),
+                        Text('Disponível'),
+                      ],
+                    ),
+                  ],
+                ),
+                margin: EdgeInsets.all(15.0),
+              ),
+            ),
+            margin: EdgeInsets.fromLTRB(15.0, 13.0, 15.0, 18.0),
+          ),
+          Divider(),
+          Container(
+            child: Card(
+              elevation: 7,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              color: Colors.grey[900],
+              shadowColor: Colors.yellow,
+              child: Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Image(
+                      image: AssetImage('img/camiseta.jpeg'),
+                      height: 140.0,
+                    ),
+                    Column(
+                      children: [
+                        Text('Camiseta masculina'),
+                        Text('R\$ 30.00',
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold)),
+                        ButtonBar(
+                          // margin:
+                          alignment: MainAxisAlignment.center,
+                          buttonHeight: 10,
+                          //mainAxisSize: MainAxisSize.max,
+                          buttonPadding: EdgeInsets.all(5),
+                          children: <Widget>[
+                            RaisedButton(
+                              color: Colors.red,
+                              child: Row(
+                                children: [
+                                  Icon(Icons.shopping_basket_outlined),
+                                  Text('Comprar'),
+                                ],
+                              ),
+                              onPressed: () {},
+                            ),
+                          ],
+                        ),
+                        Text('Disponível'),
+                      ],
+                    ),
+                  ],
+                ),
+                margin: EdgeInsets.all(15.0),
+              ),
+            ),
+            margin: EdgeInsets.all(15.0),
+          ),
+        ],
+      ),
     );
   }
 }
